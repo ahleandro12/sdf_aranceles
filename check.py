bbee4d4b0eca8ca1bea88d831e5f3e4019dd519d
Leandro Apodaca <ahleandro.ha@gmail.com>
2026-05-26 20:49:41 -0300

    Remove check.py

--- check.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('data/demo.db')
-try:
-    rows = conn.execute("SELECT producto, kg, forwarder_kg FROM matriz_costos").fetchall()
-    for r in rows:
-        print(r)
-except Exception as e:
-    print("Error:", e)
